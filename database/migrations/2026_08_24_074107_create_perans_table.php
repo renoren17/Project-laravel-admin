19b540d047dd849672ae278ff42085f1a0d5697f
Darapermatasarii <darapermatasari21@gmail.com>
2026-08-24 15:52:08 +0700

    Menambahkan Table Peran

---
+++ database/migrations/2026_08_24_074107_create_perans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('perans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 45);
+            
+            // Format persis sesuai gambar gurumu
+            $table->unsignedBigInteger('film_id');
+            $table->foreign('film_id')->references('id')->on('films');
+
+            $table->unsignedBigInteger('cast_id');
+            $table->foreign('cast_id')->references('id')->on('casts');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('perans');
+    }
+};
