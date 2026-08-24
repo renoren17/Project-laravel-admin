d7885b11b16075b2861edd6d3ba750f5159ea413
wildanwhiskey <idanaja2008@gmail.com>
2026-08-24 15:47:31 +0700

    Menambahkan table genres

--- database/migrations/2026_08_24_071124_create_profiles_table.php
+++ database/migrations/2026_08_24_071124_create_profiles_table.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('profiles', function (Blueprint $table) {
@@ -17,6 +20,9 @@ return new class extends Migration
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('profiles');
