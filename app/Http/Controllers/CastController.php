162ba9f6aac9e7a6d3f7a2714af20de9c4101ff5
MuhammadRakhaAssyfa <muhamadraka436@gmail.com>
2026-09-21 10:40:53 +0700

    membuat model untuk cast

--- app/Http/Controllers/CastController.php
+++ app/Http/Controllers/CastController.php
@@ -3,70 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Models\Cast;
 
 class CastController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $casts = DB::table('casts')->get();
+        $casts = Cast::all();
         return view('cast.index', compact('casts'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('cast.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        // Validasi input
         $request->validate([
         'nama'  => 'required|min:3',
         'umur'  => 'required|numeric',
         'bio'   => 'required',
         ]);
 
-        // Simpan data ke tabel casts
-        DB::table('casts')->insert([
+        Cast::create([
         'nama' => $request['nama'],
         'umur' => $request['umur'],
         'bio'  => $request['bio'],
         ]);
 
-        // Redirect ke halaman index
         return redirect()->route('cast.index')->with(['success' => 'Data Telah ditambahkan']);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        $cast = DB::table('casts')->where('id', $id)->first();
+        $cast = Cast::findOrFail($id);
         return view('cast.edit', compact('cast'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         $request->validate([
@@ -75,23 +54,20 @@ class CastController extends Controller
             'bio' => 'required',
         ]);
 
-        DB::table('casts')
-            ->where('id', $id)
-            ->update([
-                'nama' => $request['nama'],
-                'umur' => $request['umur'],
-                'bio' => $request['bio'],
-            ]);
+        $cast = Cast::findOrFail($id);
+        $cast->update([
+            'nama' => $request['nama'],
+            'umur' => $request['umur'],
+            'bio' => $request['bio'],
+        ]);
 
             return redirect()->route('cast.index')->with(['success'=>'Data berhasil diupdate!!']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        DB::table('casts')->where('id', $id)->delete();
+        $cast = Cast::findOrFail($id);
+        $cast->delete();
         return redirect()->route('cast.index')->with(['success'=>'Data berhasil dihapus']);
     }
 }
